30e30625c3cfc221824329f6c210eb034322e80a
Henry Yang <mail@xiaoming.se>
2019-02-25 16:47:28 +0100

    Change implementation of JsonNetwork to use Keras functional API

--- src/exercises/jsonnetwork.py
+++ src/exercises/jsonnetwork.py
@@ -6,13 +6,14 @@ This is an exercise where A network is built from parsing a Json config file
 '''
 
 import tensorflow as tf
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.backend.tensorflow_backend import set_session
 from keras import backend as K
 import keras.layers as nn_layers
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+from keras.datasets import mnist
 
 network_model1 = """
 {
@@ -33,13 +34,12 @@ network_model1 = """
             "units" : 1,
             "activation" : "tanh"
         }
-    ],
-    "loss" : "bernhard_energy"
+    ]
 }
 """
 network_model2 = """
 {
-    "input_shape" : [28,28,1]
+    "input_shape" : [28,28,1],
     "layers" : [
         {
             "type" : "Conv2D",
@@ -58,11 +58,10 @@ network_model2 = """
         },
         {
             "type" : "Dense",
-            "units" : "10",
+            "units" : 10,
             "activation" : "softmax"
         }
-    ],
-    "loss" : "categorical_crossentropy"
+    ]
 }
 """
 
@@ -76,41 +75,33 @@ def build_model(config_json):
     def build_layers(config):
  
         layers_configs = config["layers"]
-        layers = [nn_layers.InputLayer(input_shape=tuple(config["input_shape"]))]
+        input_layer = nn_layers.Input(shape=tuple(config["input_shape"]))
+        net_struct = input_layer
         for lc in layers_configs:
-            layers += parse_layer_conf(lc)
-        
-        return layers
+            print(lc)
+            net_struct = parse_layer_conf(lc)(net_struct)
+        return input_layer, net_struct
         
     config = json.loads(config_json)
-    layers = build_layers(config)
-    print(layers)
-    model = Sequential(layers)
-    model.compile(optimizer='adam', loss=loss_function(config['loss']), metrics=['accuracy'])
+    input_layer, net_struct = build_layers(config)
+    model = Model(inputs=input_layer,outputs=net_struct)
+
     return model
 
-def loss_function(function_name):
-    if function_name == "bernhard_energy":
-        return bernhard_energy
-    else:
-        return function_name
-
 def parse_layer_conf(layer_conf):
+    def build_conv2D(x,layer_conf):
+
+        x = nn_layers.Conv2D(layer_conf["units"],tuple(layer_conf["kernel_size"]))(x)
+        x = nn_layers.BatchNormalization(axis=-1)(x)
+        x = nn_layers.Activation(layer_conf["activation"])(x)
+        return x
     layer_type = layer_conf["type"]
     if layer_type == "Dense":
-        return [nn_layers.Dense(
-            layer_conf["units"],
-            activation = layer_conf["activation"]         
-        )]
+        return nn_layers.Dense(layer_conf["units"],activation=layer_conf["activation"])
     if layer_type == "Conv2D":
-        return [
-            nn_layers.Conv2D(
-                layer_conf["units"],
-                layer_conf["kernel_size"]
-            ),
-            nn_layers.BatchNormalization(axis=-1),
-            nn_layers.Activation(layer_conf["activation"])
-        ]
+        return lambda x: build_conv2D(x,layer_conf)
+    if layer_type == 'Flatten':
+        return nn_layers.Flatten()
     else:
         return None
     
@@ -160,7 +151,41 @@ def bernhard_energy(y_true, y_pred):
     s = K.sum(K.square(y_true - y_pred))
     return (1/2) * s
 
-xtrain,ytrain,xtest,ytest = generate_data(count=100000)
+def process_data(data_x, data_y):
+    # Normalize the data
+    ndata_x = data_x * (1/255)
+
+    # onehot encode labels
+    length_y = data_y.size
+    labels_y = np.zeros([length_y,10])
+    for i,j in zip(list(range(length_y)),data_y.astype(np.int32)):
+        labels_y[i,j] = 1
+    
+    return ndata_x,labels_y
+
+#xtrain,ytrain,xtest,ytest = generate_data(count=100000)
 setup_keras()
-model = build_model(network_model1)
-model.fit(xtrain,ytrain,verbose=1,validation_data=(xtest,ytest),epochs=50)
+#model = build_model(network_model1)
+#model.compile(optimizer='adam', loss=bernhard_energy, metrics=['accuracy'])
+#model.fit(xtrain,ytrain,verbose=1,validation_data=(xtest,ytest),epochs=50)
+
+## LOADING THE MNIST DATA
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+## RESHAPING THE DATA to suitable forms for the CNN
+x_train = x_train.reshape(60000,28,28,1)
+x_test  = x_test.reshape(10000,28,28,1)
+
+## Processing the data, and isolates a validation set
+ntrain_x, oh_trainy = process_data(x_train,y_train)
+
+randidx = np.random.permutation(y_train.size)
+val_x,val_y = ntrain_x[randidx[0:10000]],oh_trainy[randidx[0:10000]]
+train_x,train_y = ntrain_x[randidx[10000:]],oh_trainy[randidx[10000:]]
+
+model = build_model(network_model2)
+print("failing to build")
+model.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_x,train_y,verbose=1, validation_data=(val_x,val_y),epochs=5)
+
+## Testing the Model

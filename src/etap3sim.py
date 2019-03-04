121a67aff4273fe36d70e7e8b94e8e91888570d6
Henry Yang <mail@xiaoming.se>
2019-03-04 12:18:01 +0100

    Refine boilerplate code for etap3sim.py

--- src/etap3sim.py
+++ src/etap3sim.py
@@ -1,6 +1,7 @@
 import ANN as ann
 import utils
 import json
+import keras.losses as klosses
 
 network_model1 = """
 {
@@ -47,15 +48,22 @@ xtrain,ytrain,xtest,ytest = utils.load_mnist()
 xtrain = xtrain.reshape(60000,28,28,1)
 xtest = xtest.reshape(10000,28,28,1)
 
-model = ann.parse_model_js(network_model1)
+#model = ann.parse_model_js(network_model1)
+
+#model.compile(optimizer="adam", loss=ann.adveserial_loss(klosses.categorical_crossentropy,model,model.inputs), metrics=["accuracy"])
+#model.fit(xtrain,ytrain,verbose=1,validation_data=(xtest,ytest),epochs=3)
+#print("SCRIPT RUN SUCCÈSSFUL")
+
 model_conf = json.loads(network_model1)
 
 inputs, outputs, train_model, model_list, merge_model = ann.build_ensemble([model_conf])
 
 ensemble_size = len(model_list)
 
-train_model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
-train_model.fit([xtrain]*ensemble_size,[ytrain]*ensemble_size,verbose=1,validation_data=([xtest]*ensemble_size,[ytest]*ensemble_size))
+
+lossfunctions = [ann.adveserial_loss(klosses.categorical_crossentropy, m) for m in model_list]
+train_model.compile(optimizer="adam",loss=lossfunctions ,metrics=["accuracy"])
+train_model.fit([xtrain]*ensemble_size,[ytrain]*ensemble_size,verbose=1,validation_data=([xtest]*ensemble_size,[ytest]*ensemble_size),epochs=3)
 
 
 

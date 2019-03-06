64b632cbad7a1b5c63b54709b06d8b9d284b9978
Henry Yang <mail@xiaoming.se>
2019-03-06 10:51:49 +0100

    Rework Etap1 simulations to use the new Dataset by Henry Yang and a combined dataset

--- src/etap1sim.py
+++ src/etap1sim.py
@@ -9,13 +9,51 @@ from time import time
 import utils
 import numpy as np
 import matplotlib.pyplot as plt
+import ANN as ann
+
 
 trials = 5
 mnist_acc = np.zeros(trials)
-img_acc = np.zeros(trials)
-num_epochs = 5
+imgXM_acc = np.zeros(trials)
+imgOB_acc = np.zeros(trials)
+combined_acc = np.zeros(trials)
+num_epochs = 3
 
-
+network_model = """
+{
+    "input_shape" : [28,28,1],
+    "layers" : [
+        {
+            "type" : "Conv2D",
+            "units" : 32,
+            "kernel_size" : [3,3],
+            "activation" : "relu"
+        },
+        {
+            "type" : "BatchNormalization",
+            "axis" : -1
+        },
+        {
+            "type" : "MaxPooling2D",
+            "pool_size" : [2,2],
+            "strides" : [2,2]
+        },
+        {
+            "type" : "Flatten"
+        },
+        {
+            "type" : "Dense",
+            "units" : 16,
+            "activation" : "relu"
+        },
+        {
+            "type" : "Dense",
+            "units" : 10,
+            "activation" : "softmax"
+        }
+    ]
+}
+"""
 
 def test_model(model, test_data, test_labels):
     predictions = model.predict(test_data)
@@ -25,54 +63,44 @@ def test_model(model, test_data, test_labels):
 
 utils.setup_gpu_session()
 
-path = os.path.join(".","images","60 Images")
+pathXM = os.path.join(".","images","XiaoMing_Digits")
+pathOB = os.path.join(".","images","60 Images")
 
-imgs,labels = utils.load_image_data(path)
+imgsXM,labelsXM = utils.load_image_data(pathXM)
+imgsOB,labelsOB = utils.load_image_data(pathOB)
 
-(x_train,y_train),(x_test,y_test) = mnist.load_data()
+xtrain,ytrain,xtest,ytest = utils.load_mnist(normalize=True)
+xtrain,xtest = xtrain.reshape(60000,28,28,1),xtest.reshape(10000,28,28,1)
 
-x_train = x_train.reshape(60000,28,28,1)
-x_test  = x_test.reshape(10000,28,28,1)
-
-x_train = utils.normalize(x_train)
-y_train = utils.create_one_hot(y_train)
-
-x_test = utils.normalize(x_test)
-y_test = utils.create_one_hot(y_test)
-
-(count,features) = imgs.shape
-
-imgs = imgs.reshape(count,28,28,1)
-
-x_img = utils.normalize(imgs)
-y_img = utils.create_one_hot(labels)
+img_size = imgsXM.shape[0]
+ximgXM = imgsXM.reshape(img_size,28,28,1)
+ximgXM = utils.normalize_data(ximgXM)
+yimgXM = utils.create_one_hot(labelsXM)
+img_size = imgsOB.shape[0]
+ximgOB = imgsOB.reshape(img_size,28,28,1)
+ximgOB = utils.normalize_data(ximgOB)
+yimgOB = utils.create_one_hot(labelsOB)
+imgs_combined = np.concatenate((ximgXM,ximgOB))
+labels_combined = np.concatenate((yimgXM,yimgOB))
 
 for i in range(trials):
 
-    randidx = np.random.permutation(int(y_train.size / 10))
-    val_x,val_y = x_train[randidx[0:10000]],y_train[randidx[0:10000]]
-    train_x,train_y = x_train[randidx[10000:]],y_train[randidx[10000:]]
+    t_xtrain,t_ytrain,xval,yval = utils.create_validation(xtrain,ytrain)
 
-    layers_list = [
-        nn_layers.Conv2D(64, (3,3), input_shape=(28,28,1)),
-        nn_layers.BatchNormalization(axis=-1),
-        nn_layers.Activation('relu'),
-        nn_layers.Conv2D(32, (3,3)),
-        nn_layers.BatchNormalization(axis=-1),
-        nn_layers.Activation('relu'),
-        nn_layers.Flatten(),
-        nn_layers.Dense(10, activation='softmax')
-    ]
+    
 
     ## Realizing and training the data
     ## uses TensorBoard to monitor the progress
-    model = Sequential(layers_list)
+    model = ann.parse_model_js(network_model)
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
-    model.fit(train_x,train_y,verbose=1, validation_data=(val_x,val_y),epochs=num_epochs)
+    model.fit(t_xtrain,t_ytrain,verbose=1, validation_data=(xval,yval),epochs=num_epochs)
 
-    mnist_acc[i] = test_model(model, x_test,y_test)
-    img_acc[i] = test_model(model, x_img, y_img)
+    mnist_acc[i] = test_model(model, xtest,ytest)
+    imgXM_acc[i] = test_model(model, ximgXM, yimgXM)
+    imgOB_acc[i] = test_model(model, ximgOB, yimgOB)
+    combined_acc[i] = test_model(model, imgs_combined, labels_combined)
+    
 
 Xaxis = np.arange(trials)
 
@@ -85,10 +113,27 @@ mnist_line, = plt.plot(Xaxis, mnist_acc,color="blue",marker="x",linestyle="-")
 mnist_avg_line, = plt.plot(Xaxis, np.ones(trials) * np.mean(mnist_acc), color="red",linestyle="--")
 plt.legend((mnist_line, mnist_avg_line),("MNIST Accuracy", "MNIST Average Accuracy"))
 plt.subplot(2,1,2)
-plt.title("accuracy test of own Imgs")
+plt.title("accuracy test of Combined Dataset")
 plt.xlabel("trials")
 plt.ylabel("accuracy")
-imgs_line, = plt.plot(Xaxis, img_acc,color="blue",marker="x",linestyle="-")
-imgs_avg_line, = plt.plot(Xaxis, np.ones(trials) * np.mean(img_acc), color="red",linestyle="--")
+imgs_line, = plt.plot(Xaxis, combined_acc,color="blue",marker="x",linestyle="-")
+imgs_avg_line, = plt.plot(Xaxis, np.ones(trials) * np.mean(combined_acc), color="red",linestyle="--")
 plt.legend((imgs_line, imgs_avg_line), ("IMGS Accuracy", "IMGS Average Accuracy"))
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.title("accuracy test of Olek's Digits")
+plt.xlabel("trials")
+plt.ylabel("accuracy")
+mnist_line, = plt.plot(Xaxis, imgOB_acc,color="blue",marker="x",linestyle="-")
+mnist_avg_line, = plt.plot(Xaxis, np.ones(trials) * np.mean(imgOB_acc), color="red",linestyle="--")
+plt.legend((mnist_line, mnist_avg_line),("MNIST Accuracy", "MNIST Average Accuracy"))
+plt.subplot(2,1,2)
+plt.title("accuracy test of Henry's Digits")
+plt.xlabel("trials")
+plt.ylabel("accuracy")
+imgs_line, = plt.plot(Xaxis, imgXM_acc,color="blue",marker="x",linestyle="-")
+imgs_avg_line, = plt.plot(Xaxis, np.ones(trials) * np.mean(imgXM_acc), color="red",linestyle="--")
+plt.legend((imgs_line, imgs_avg_line), ("IMGS Accuracy", "IMGS Average Accuracy"))
+
 plt.show()

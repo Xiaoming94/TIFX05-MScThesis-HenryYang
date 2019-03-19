c539198bc079d6a979e863a5a6e49ba031129254
Henry Yang <mail@xiaoming.se>
2019-03-19 22:30:37 +0100

    Add classify function that takes some predictions and classifies them

--- src/ANN/__init__.py
+++ src/ANN/__init__.py
@@ -1,3 +1,3 @@
 from .model_builder import build_model, build_layers, parse_model_js, build_ensemble
 from .loss_functions import *
-from .model_tester import test_model
+from .model_tester import test_model, classify
--- src/ANN/model_tester.py
+++ src/ANN/model_tester.py
@@ -9,7 +9,13 @@ def test_model(model, test_data, test_labels, metric = "accuracy"):
     
     predictions = model.predict(test_data)
     return metric(predictions, test_labels)
-        
+
+def classify(pred):
+    class_index = np.argmax(pred,1)
+    classes = np.zeros(pred.shape)
+    for i,j in zip(range(class_index.size),class_index):
+        classes[i,j] = 1
+    return classes   
     
 def test_accuracy(test_pred, test_labels):
     correct = np.equal(np.argmax(test_pred,1),np.argmax(test_labels,1))
@@ -18,6 +24,7 @@ def test_accuracy(test_pred, test_labels):
 
 def test_classification_err(test_pred, test_labels):
     num_data = test_pred.shape[0]
-    diff = test_pred - test_labels
+    classes = classify(test_pred)
+    diff = classes - test_labels
     c_err = (1/(2 * num_data)) * np.sum(np.sum(np.abs(diff)))
     return c_err
